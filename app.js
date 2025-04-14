7c0b814ffefefaca0cc86dc3c833def1332598f7
Shivam9973-hub <shivam7258kumar@gmail.com>
2025-04-14 16:08:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,20 @@
-let userscore=0;
-let compscore=0;
-const message=document.getElementById("msg");
-const score1=document.getElementById("comp-score");
-const score2=document.getElementById("user-score");
+let string="";
+let buttons=document.querySelectorAll(".button");
 
-
-const choices1=document.querySelectorAll(".choice");
-const gencompchoice=()=>{
-    const option=["rock","paper","scissor"];
-    const index=Math.floor(Math.random()*3);
-    return option[index];
-}
-const draw=()=>{
-    message.innerText="game was draw";
-    message.style.backgroundColor="black"
-}
-const playgame=(userchoice)=>{
-   console.log("userchoice=",userchoice);
-   const compchoice=gencompchoice();
-   console.log("compchoice=",compchoice);
-   if(compchoice===userchoice){
-    console.log("game was draw")
-    draw()
-   }
-   else if(userchoice==="rock"&&compchoice==="scissor"||userchoice==="paper"&&compchoice==="rock"||userchoice==="scissor"&&compchoice==="paper"){
-  console.log("you win");
-    userscore++;
-    score2.innerText=userscore++
-  message.innerText=`you win.${userchoice} beats ${compchoice}`
-  message.style.backgroundColor="green"
-
-   }
-   else{
-    console.log("you lose");
-    compscore++;
-    score1.innerText=compscore++
-    message.innerText=`you lose.${compchoice} beats ${userchoice}`
-  message.style.backgroundColor="red"
-   }
-}
-
-choices1.forEach((choice)=>{
-    choice.addEventListener("click",()=>{
-     const userchoice=choice.getAttribute("id");
-     playgame(userchoice)
-   
-    });
-});
+buttons.forEach((button)=>{
+   button.addEventListener("click",(e)=>{
+    // console.log(e.target)
+    if(e.target.innerHTML==="c"){
+        string=""
+        document.querySelector("input").value=string
+    }
+    else if(e.target.innerHTML==="="){
+        string=eval(string);
+        document.querySelector("input").value=string
+    }
+    else{
+    string=string+e.target.innerHTML;
+    document.querySelector("input").value=string;
+    }
+   })
+})
